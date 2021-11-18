d0e8ed5bb010ce73fa2e5ae84c39a5a8c0899cd7
Daniel Maciejewski <46847771+DanielWMaciejewski@users.noreply.github.com>
2021-11-17 19:33:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("HexaPy")
+"""
+Created by Daniel Maciejewski 11/17/2021
+INFOST 691-202 Digital Forensics Final Project
+
+Assignment Prompt
+-----------------
+Using your favorite programming language, create a software tool (application) that would act as a 
+hex editor. That is. The tool would be able to open any type of files at the binary level similar to 
+the hexedit tool we learned in this course.
+
+Sources Cited
+-------------
+https://www.qtrac.eu/pyhexviewer.html
+/end Comments
+"""
+#imports
+import os, sys
+import tkinter as tk
+
+print("HexaPy")#Project Name
