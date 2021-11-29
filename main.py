d578fd19f28970325cd4f550f8d9a546742f26fe
Daniel Maciejewski <46847771+DanielWMaciejewski@users.noreply.github.com>
2021-11-29 14:30:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,19 +10,21 @@ the hexedit tool we learned in this course.
 
 Sources Cited
 -------------
-https://www.qtrac.eu/pyhexviewer.html
 https://www.youtube.com/watch?v=kJSOuPTjH50
-/end Comments
 """
+#imports
+import os
 #Variable Declaration
 
 bytesRead = 0 #count the number of bytes being read in the 'for' loop
 contentLine = [] #list (array) holds the contents of the 'for' loop
+lowerAscii = 32#the lower range of human-readable ascii
+upperAscii = 126#the upper range of human-readable ascii
+print("=================================================================================================================================================\nHexaPy: Feed me a file and I'll feed you its hex content! 0w0",
+"\n=================================================================================================================================================\n")#Project Name
 
-print("HexaPy")#Project Name
 
-
-file = input("Enter a file name: ") #Create a string to hold the name of the target file
+file = input("file name: ") #Create a string to hold the name of the target file
 
 """
 use python open() method to open chosen file in binary format
@@ -37,19 +39,24 @@ for byte in fileContent:#The 'for' loop prints out the data read from file
     contentLine.append(byte)#add the contents read from byte to contentLine list
     """
     print() notes
-    overview: display outputted line in console as a formatted printout 
+    overview: display outputted line in console as a formatted hex printout 
     -the format(byte,2) method displays the string (byte) placeholder to be passed in to print with several modifications
         -'{}' delimits the formatted output
         -':x' reads the output as lower case hex format
         -'end=" "' parameter places an empty space at the end of the printed line rather than default newline character
     """
     print("{0:0{1}x}".format(byte,2), end=" ")
-    if bytesRead % 16 == 0:
+    if bytesRead % 16 == 0:#if the line has 16 characters outputted mark the end
         print("#",end=" ")
-        for byteTwo in contentLine:
-            if (byteTwo >= 32) and (byteTwo <= 126):
-                print(chr(byteTwo), end = "")
+        """
+        this 'for' loop reads content line and dumps human-readable characters from the ascii values of each hex code and omits assembly instructions
+        """
+        for asciiNumber in contentLine:
+            if (asciiNumber >= lowerAscii) and (asciiNumber <= upperAscii):
+                print(chr(asciiNumber), end = "")
             else:
                 print("*", end=" ") 
         contentLine = []
         print("")
+print("\n=================================================================================================================================================\n",
+"Yum ^w^! Hex Dump Complete\n================================================================================================================================================\n")
